9e2815243e8fd569f7b74eb114337a96b107a015
Marcin Forysinski <marcin.forysinski@freshmail.pl>
2019-09-30 11:22:41 +0200

    Simple list MVP

--- src/App.js
+++ src/App.js
@@ -1,9 +1,78 @@
 import React, { Component } from "react";
+import { Paper, Typography, TextField, Button } from "@material-ui/core";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton
+} from "@material-ui/core";
+
+import { Delete } from "@material-ui/icons";
 
 class App extends Component {
-  state = { exercises: [], title: "" };
+  state = {
+    list: [
+      { id: 1, title: "Bench Press" },
+      { id: 2, title: "Deadlift" },
+      { id: 3, title: "Squats" }
+    ],
+    title: ""
+  };
+  handleChange = ({ target: { name, value } }) =>
+    this.setState({ [name]: value });
+
+  handleCreate = e => {
+    e.preventDefault();
+    if (this.state.title) {
+      this.setState(({ list, title }) => ({
+        list: [{ title, id: Date.now() }, ...list],
+        title: ""
+      }));
+    }
+  };
+  handleDelete = id => {
+    this.setState(({ list }) => ({ list: list.filter(li => li.id !== id) }));
+  };
+
   render() {
-    return <h1>Simple Lisst</h1>;
+    const { title, list } = this.state;
+    return (
+      <Paper>
+        <Typography variant="h4" align="center" gutterBottom>
+          Simple List
+        </Typography>
+
+        <form onSubmit={this.handleCreate}>
+          <TextField
+            name="title"
+            label="Simple List"
+            value={title}
+            onChange={this.handleChange}
+            margin="normal"
+          />
+
+          <Button type="submit" color="primary" variant="contained">
+            Create
+          </Button>
+        </form>
+        <List>
+          {list.map(({ id, title }) => (
+            <ListItem key={id}>
+              <ListItemText primary={title} />
+              <ListItemSecondaryAction>
+                <IconButton
+                  color="primary"
+                  onClick={() => this.handleDelete(id)}
+                >
+                  <Delete />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          ))}
+        </List>
+      </Paper>
+    );
   }
 }
 
