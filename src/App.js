396128c79531c5f39576cc63996c487941539f26
Marcin Forysinski <marcin.forysinski@freshmail.pl>
2019-09-30 12:40:44 +0200

    Added Styled Components and Material UI Theming

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React, { Component } from "react";
-import { Paper, Typography, TextField, Button } from "@material-ui/core";
+import {} from "@material-ui/core";
 import {
   List,
   ListItem,
   ListItemText,
   ListItemSecondaryAction,
-  IconButton
+  IconButton,
+  Paper,
+  Typography,
+  TextField,
+  Button
 } from "@material-ui/core";
 
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
 import { Delete } from "@material-ui/icons";
+import { StylesProvider } from "@material-ui/styles";
+import styled from "styled-components";
+import { pink } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: pink,
+    type: "dark"
+  }
+});
+
+const StyledPaper = styled(Paper)`
+  max-width: 400px;
+  margin: 2em;
+  padding: 1em;
+`;
+const InlineTextField = styled(TextField)`
+  margin-top: 0;
+  margin-bottom: 0;
+`;
+const Form = styled.form`
+  display: flex;
+  justify-content: space-evenly;
+  margin: 2em auto;
+`;
 
 class App extends Component {
   state = {
     list: [
-      { id: 1, title: "Bench Press" },
-      { id: 2, title: "Deadlift" },
-      { id: 3, title: "Squats" }
+      { id: 1, title: "React.js" },
+      { id: 2, title: "Material UI" },
+      { id: 3, title: "Styled Components" },
+      { id: 4, title: "Are awesome!" }
     ],
     title: ""
   };
@@ -38,40 +70,44 @@ class App extends Component {
   render() {
     const { title, list } = this.state;
     return (
-      <Paper>
-        <Typography variant="h4" align="center" gutterBottom>
-          Simple List
-        </Typography>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <StyledPaper>
+            <Typography variant="h4" align="center" gutterBottom>
+              Simple List
+            </Typography>
 
-        <form onSubmit={this.handleCreate}>
-          <TextField
-            name="title"
-            label="Simple List"
-            value={title}
-            onChange={this.handleChange}
-            margin="normal"
-          />
+            <Form onSubmit={this.handleCreate}>
+              <InlineTextField
+                name="title"
+                label="Simple List"
+                value={title}
+                onChange={this.handleChange}
+                margin="normal"
+              />
 
-          <Button type="submit" color="primary" variant="contained">
-            Create
-          </Button>
-        </form>
-        <List>
-          {list.map(({ id, title }) => (
-            <ListItem key={id}>
-              <ListItemText primary={title} />
-              <ListItemSecondaryAction>
-                <IconButton
-                  color="primary"
-                  onClick={() => this.handleDelete(id)}
-                >
-                  <Delete />
-                </IconButton>
-              </ListItemSecondaryAction>
-            </ListItem>
-          ))}
-        </List>
-      </Paper>
+              <Button type="submit" color="primary" variant="contained">
+                Create
+              </Button>
+            </Form>
+            <List>
+              {list.map(({ id, title }) => (
+                <ListItem key={id}>
+                  <ListItemText primary={title} />
+                  <ListItemSecondaryAction>
+                    <IconButton
+                      color="primary"
+                      onClick={() => this.handleDelete(id)}
+                    >
+                      <Delete />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              ))}
+            </List>
+          </StyledPaper>
+        </ThemeProvider>
+      </StylesProvider>
     );
   }
 }
